0316e374119442547c19d99b997cf8b93377cd41
nahuetomas <nrodriguez@portinos.com.ar>
2022-11-29 22:47:40 -0300

    [FIX] Carteles andando

--- funciones/graficos/cartel.py
+++ funciones/graficos/cartel.py
@@ -15,37 +15,40 @@ def cartel(screen, titulo, colorFont, fontSize, subtitle=''):
     cartel = pygame.image.load('assets/img/CARTEL.jpg')
     screen.blit(cartel, (posX, posY))
 
-    centerCartel = cartel.get_rect().center
-
     mensajeG = defaultFontGrande.render(titulo, 1, colorFont)
     width = mensajeG.get_width()
-    height = mensajeG.get_height()
     screen.blit(mensajeG, (center[0] - width // 2, 120))
     if (subtitle):
         subtitleG = pygame.font.Font(
             pygame.font.get_default_font(), 20).render(subtitle, 1, colorFont)
         width = subtitleG.get_width()
         screen.blit(
-            subtitleG, (centerCartel[0] - (width // 2), centerCartel[1] - 20))
+            subtitleG, (center[0] - width // 2, 180))
 
     exitBtn = pygame.image.load('assets/img/BTN-SALIR.jpg')
     screen.blit(exitBtn, (80, 340))
+    exitBtnRect = pygame.draw.rect(
+        screen, 'white', pygame.Rect(80, 340, 80, 40), 1)
 
     playAgainBtn = pygame.image.load('assets/img/BTN-JDN.jpg')
     screen.blit(playAgainBtn, (600, 340))
+    playAgainBtnRect = pygame.draw.rect(
+        screen, 'white', pygame.Rect(600, 340, 80, 40), 1)
 
     menuBtn = pygame.image.load('assets/img/BTN-MENU.jpg')
-    screen.blit(playAgainBtn, (350, 340))
+    screen.blit(menuBtn, (350, 340))
+    menuBtnRect = pygame.draw.rect(
+        screen, 'white', pygame.Rect(350, 340, 80, 40), 1)
 
     ev = pygame.event.get()
     for event in ev:
         if event.type == MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            if (exitBtn.collidepoint(pos)):
+            if (exitBtnRect.collidepoint(pos)):
                 pygame.quit()
-            if (playAgainBtn.collidepoint(pos)):
+            if (playAgainBtnRect.collidepoint(pos)):
                 return 'reset'
-            if (menuBtn.collidepoint(pos)):
+            if (menuBtnRect.collidepoint(pos)):
                 return 'menu'
         if event.type == QUIT:
             pygame.quit()
