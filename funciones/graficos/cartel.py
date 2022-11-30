0be4972121e0ec2028d941c69e587e5c8c38720f
nahuetomas <nrodriguez@portinos.com.ar>
2022-11-29 22:25:04 -0300

    [UPDATE] IMG de carteles

--- funciones/graficos/cartel.py
+++ funciones/graficos/cartel.py
@@ -5,21 +5,22 @@ from config.configuracion import COLOR_CORRECTA, COLOR_INCORRECTA, COLOR_CARTEL,
 
 
 # CREACION DEL CARTEL FINAL
-def cartel(screen, titulo, colorFont, colorCartel, fontSize, subtitle=''):
+def cartel(screen, titulo, colorFont, fontSize, subtitle=''):
     defaultFontGrande = pygame.font.Font(
         pygame.font.get_default_font(), fontSize)
     screen_rect = screen.get_rect()
     center = screen_rect.center
     posX = center[0] - 350
     posY = center[1] - 200
-    rect = pygame.Rect(posX, posY, 700, 300)
-    centerCartel = rect.center
-    pygame.draw.rect(screen, colorCartel, rect, 0)
+    cartel = pygame.image.load('assets/img/CARTEL.jpg')
+    screen.blit(cartel, (posX, posY))
+
+    centerCartel = cartel.get_rect().center
 
     mensajeG = defaultFontGrande.render(titulo, 1, colorFont)
     width = mensajeG.get_width()
-    screen.blit(
-        mensajeG, (centerCartel[0] - (width // 2), centerCartel[1] - 80))
+    height = mensajeG.get_height()
+    screen.blit(mensajeG, (center[0] - width // 2, 120))
     if (subtitle):
         subtitleG = pygame.font.Font(
             pygame.font.get_default_font(), 20).render(subtitle, 1, colorFont)
@@ -27,14 +28,16 @@ def cartel(screen, titulo, colorFont, colorCartel, fontSize, subtitle=''):
         screen.blit(
             subtitleG, (centerCartel[0] - (width // 2), centerCartel[1] - 20))
 
-    exitBtn = pygame.draw.rect(screen, 'red', pygame.Rect(80, 340, 80, 40), 0)
-    playAgainBtn = pygame.draw.rect(
-        screen, 'green', pygame.Rect(600, 340, 80, 40), 0)
-    menuBtn = pygame.draw.rect(
-        screen, 'orange', pygame.Rect(350, 340, 80, 40), 0)
+    exitBtn = pygame.image.load('assets/img/BTN-SALIR.jpg')
+    screen.blit(exitBtn, (80, 340))
+
+    playAgainBtn = pygame.image.load('assets/img/BTN-JDN.jpg')
+    screen.blit(playAgainBtn, (600, 340))
+
+    menuBtn = pygame.image.load('assets/img/BTN-MENU.jpg')
+    screen.blit(playAgainBtn, (350, 340))
 
     ev = pygame.event.get()
-
     for event in ev:
         if event.type == MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
@@ -51,15 +54,13 @@ def cartel(screen, titulo, colorFont, colorCartel, fontSize, subtitle=''):
 
 
 def cartelGanar(screen):
-    return cartel(screen, "GANASTE!!!", COLOR_CORRECTA,
-                  COLOR_CARTEL, TAMANNO_LETRA_GRANDE)
+    return cartel(screen, "GANASTE!!!", COLOR_CORRECTA, TAMANNO_LETRA_GRANDE)
 
 
 # Cartel PERDER
 def cartelPerder(screen, palabraCorrecta):
-    return cartel(screen, "PERDISTE!!",
-                  COLOR_INCORRECTA, COLOR_CARTEL, TAMANNO_LETRA_GRANDE, "La palabra correcta era: " + palabraCorrecta.upper())
+    return cartel(screen, "PERDISTE!!", COLOR_INCORRECTA, TAMANNO_LETRA_GRANDE, "La palabra correcta era: " + palabraCorrecta.upper())
 
 
 def cartelTermino(screen, cant_palabras):
-    return cartel(screen, 'TERMINO EL TIEMPO!', COLOR_CORRECTA, COLOR_CARTEL, TAMANNO_LETRA_GRANDE, "Acertaste " + cant_palabras + ' palabras :D')
+    return cartel(screen, 'TERMINO EL TIEMPO!', COLOR_CORRECTA, TAMANNO_LETRA_GRANDE, "Acertaste " + cant_palabras + ' palabras :D')
